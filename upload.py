a1de0056e860cfcb6633a61bf04ad8ea9905c2e6
dafnevk <d.vankuppevelt@esciencecenter.nl>
2017-11-15 16:11:08 +0100

    Updated the docker so that it uses our version

--- upload.py
+++ upload.py
@@ -7,7 +7,7 @@ with open('./formdata.json') as f:
 
 c = pycurl.Curl()
 post_fields = urlencode(post_data)
-#post_fields = post_data
 c.setopt(c.POSTFIELDS, post_fields)
-c.setopt(c.URL, 'http://ncwms:ncwms@localhost/ncWMS/admin/updateConfig')
+#c.setopt(c.URL, 'http://ncwms:ncwms@localhost/ncWMS/admin/updateConfig')
+c.setopt(c.URL, 'http://ncwms:ncwms@localhost/ncWMS/admin/addData')
 c.perform()
